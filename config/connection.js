8193462d8b18edc148489b0f8990c802ce9a6534
Tenshi541 <angelzavaleta541@gmail.com>
2023-05-01 21:30:52 -0700

    400 error fixed

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/Social-Network-API', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socials-network-api', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
